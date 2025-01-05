807718bdc86d2cc0716dd2f9e170831f07e2e434
loremdev2 <singanamalaaman@gmail.com>
2025-01-05 20:14:02 +0530

    Changed layout

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,12 +1,37 @@
+import React from 'react';
+
 const AuthLayout = ({
     children
 }: {
     children: React.ReactNode
 }) => {
     return (
-        <div className="h-full flex items-center justify-center  bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-800 via-gray-900 to-black text-gray-100">
-            {children}
-        </div>
+        <>
+            <style>
+                {`
+                    @keyframes gradientAnimation {
+                        0% {
+                            background-position: 0% 50%;
+                        }
+                        50% {
+                            background-position: 100% 50%;
+                        }
+                        100% {
+                            background-position: 0% 50%;
+                        }
+                    }
+
+                    .bg-animated-gradient {
+                        background: radial-gradient(ellipse at top, var(--tw-gradient-stops));
+                        background-size: 200% 200%;
+                        animation: gradientAnimation 8s ease infinite;
+                    }
+                `}
+            </style>
+            <div className="h-full flex items-center justify-center bg-animated-gradient text-gray-100">
+                {children}
+            </div>
+        </>
     );
 }
 
